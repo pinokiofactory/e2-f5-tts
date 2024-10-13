f1d8aa0998d8a601c80383d3710af042917dcd52
gepeto <gepeto@pinokio.computer>
2024-10-13 11:23:29 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# e2-f5-tts
+
+A pinokio script for https://huggingface.co/spaces/mrfakename/E2-F5-TTS
+
---
+++ install.js
@@ -0,0 +1,43 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://huggingface.co/spaces/mrfakename/E2-F5-TTS app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install gradio devicetorch",
+          "pip install -r requirements.txt"
+        ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    }
+  ]
+}
