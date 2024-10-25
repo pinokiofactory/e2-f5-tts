52511f24f67e5b2fb941215baca9e09414ef9b6a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-25 17:53:20 -0400

    update

--- update.js
+++ update.js
@@ -10,5 +10,19 @@ module.exports = {
       path: "app",
       message: "git pull"
     }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",                // Edit this to customize the venv folder path
+      path: "app",                // Edit this to customize the path to start the shell from
+      message: [
+        "pip install -e ."
+      ]
+    }
+  }, {
+    method: "fs.link",
+    params: {
+      venv: "app/env"
+    }
   }]
 }
