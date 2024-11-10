850c9f7d38809dbbc37b6533fcef6dbc9db10403
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-10 08:31:32 -0500

    mps fallback

--- train.js
+++ train.js
@@ -5,7 +5,9 @@ module.exports = {
       method: "shell.run",
       params: {
         venv: "env",                // Edit this to customize the venv folder path
-        env: { },                   // Edit this to customize environment variables (see documentation)
+        env: {
+          "PYTORCH_ENABLE_MPS_FALLBACK": 1
+        },                   // Edit this to customize environment variables (see documentation)
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "f5-tts_finetune-gradio"
