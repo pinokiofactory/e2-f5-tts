4c19b4a0377271b80611865e6bb89f9b7549a617
6Morpheus6 <wanton-decay@gmx.net>
2025-04-24 20:07:25 +0200

    remove fs.link, install with uv

--- install.js
+++ install.js
@@ -28,15 +28,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -e ."
+          "uv pip install -e ."
         ]
-      }
-    },
-    {
-      when: "{{platform !== 'linux'}}",
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
