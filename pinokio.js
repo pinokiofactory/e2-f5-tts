df1d806f4b9f59b89d58aa8ea257c243d2d97e4f
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-24 20:26:39 +0200

    script version 3.7

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  version: "3.0",
+  version: "3.7",
   title: "e2-f5-tts",
   description: "F5-TTS: A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching https://huggingface.co/spaces/mrfakename/E2-F5-TTS",
   icon: "icon.png",
